a5602ade328e3196f1fc4005f633bedf9ea0906b
Aprilssky <18782740887@139.com>
2026-05-17 10:21:40 +0000

    Fix: use var so modules can see BACKEND_URL

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // === RictWorld Backend Configuration ===
 //
 // 后端通过 cpolar 内网穿透暴露的公网 HTTPS 地址
+// 使用 var 确保模块作用域可见
 
-const BACKEND_URL = "https://6e402cc0.r6.cpolar.top";
+var BACKEND_URL = "https://6e402cc0.r6.cpolar.top";
